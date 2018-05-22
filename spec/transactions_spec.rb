947c5ab61d276a31df2b145ba8c96fdc961a64a2
Louis Moselhi <moselhil@gmail.com>
2018-05-22 14:25:19 +0100

    added test and file for transaction

---
+++ spec/transactions_spec.rb
@@ -0,0 +1,11 @@
+require './lib/transaction'
+require './lib/bank_account'
+
+
+describe Transaction do
+  it 'shows a transaction'do
+    acc = BankAccount.new
+    acc.credit(100)
+    expect(subject.transaction_history).to eq ([5])
+  end
+end
